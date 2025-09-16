3018537f8ee072d7113bfe71dd4486ca8f9f72cb
kllxs <1164601554@qq.com>
2025-09-16 20:06:31 +0800

    更新

--- src/Base.php
+++ src/Base.php
@@ -23,8 +23,7 @@ abstract class Base
     {
         if (!isset(self::$ffi)) {
             $headerPath = __DIR__ . '/PebView.h';
-            // $dllPath = self::getLibFilePath();
-            $dllPath = dirname(__DIR__) . '\lib\windows\PebView.dll'; 
+            $dllPath = self::getLibFilePath();
 
             $libHeader = file_get_contents($headerPath);
             self::$ffi = \FFI::cdef($libHeader, $dllPath);
@@ -41,21 +40,59 @@ abstract class Base
      * @return string 包含 Raylib 库文件的完整路径
      * @throws \RuntimeException 如果当前操作系统不被支持
      */
-    /* protected static function getLibFilePath(): string
+    protected static function getLibFilePath(): string
     {
         // 判断当前系统是windows还是linux
         if (PHP_OS_FAMILY === 'Windows') {
-            // 返回 Windows 系统下的 Raylib 动态链接库文件路径
-            return dirname(__DIR__) . '/build/lib/windows/raylib.dll';
+            // 返回 Windows 系统下的 PebView 动态链接库文件路径
+            return dirname(__DIR__) . DIRECTORY_SEPARATOR
+                . 'build' . DIRECTORY_SEPARATOR 
+                . 'lib' . DIRECTORY_SEPARATOR 
+                . 'windows' . DIRECTORY_SEPARATOR 
+                . 'PebView.dll';
         } else if (PHP_OS_FAMILY === 'Linux') {
-            // 返回 Linux 系统下的 Raylib 共享库文件路径
-            return dirname(__DIR__) . '/build/lib/linux/libraylib.so';
+            // 判断架构是否为x86_64
+            if (PHP_INT_SIZE === 8) {
+                $arch = 'x86_64';
+            }
+            // 判断架构是否为arrch64
+            else if (PHP_INT_SIZE === 4) {
+                $arch = 'aarch64';
+            }
+            // 若架构未知，抛出异常
+            else {
+                throw new \RuntimeException("Unsupported architecture: " . PHP_INT_SIZE . " bits");
+            }
+            // 返回 Linux 系统下的 PebView 共享库文件路径
+            return dirname(__DIR__) . DIRECTORY_SEPARATOR
+                . 'build' . DIRECTORY_SEPARATOR 
+                . 'lib' . DIRECTORY_SEPARATOR 
+                . 'linux' . DIRECTORY_SEPARATOR 
+                . $arch . DIRECTORY_SEPARATOR 
+                . 'libPebView.so';
         } elseif (PHP_OS_FAMILY === 'Darwin') {
-            // 返回 macOS 系统下的 Raylib 共享库文件路径
-            return dirname(__DIR__) . '/build/lib/macos/libraylib.dylib';
+            // 判断架构是否为x86_64
+            if (PHP_INT_SIZE === 8) {
+                $arch = 'x86_64';
+            }
+            // 判断架构是否为arm64
+            else if (PHP_INT_SIZE === 4) {
+                $arch = 'arm64';
+            }
+            // 若架构未知，抛出异常
+            else {
+                throw new \RuntimeException("Unsupported architecture: " . PHP_INT_SIZE . " bits");
+            }
+            // 返回 macOS 系统下的 PebView 共享库文件路径
+            return dirname(__DIR__) . DIRECTORY_SEPARATOR
+                . 'build' . DIRECTORY_SEPARATOR 
+                . 'lib' . DIRECTORY_SEPARATOR 
+                . 'macos' . DIRECTORY_SEPARATOR 
+                . $arch . DIRECTORY_SEPARATOR 
+                . 'libPebView.dylib';
         } else {
             // 若当前操作系统不被支持，抛出异常
             throw new \RuntimeException("Unsupported operating system: " . PHP_OS_FAMILY . ": " . PHP_OS . "");
         }
-    } */
+    }
 }
--- src/PebView.h
+++ src/PebView.h
@@ -23,12 +23,8 @@ webview_error_t webview_return(webview_t w, const char *id, int status, const ch
 int set_icon(const void* ptr, const char* iconFilePath);
 
 // dialog
-typedef void osdialog_message_callback(int result, void* user);
 int osdialog_message(int level, int buttons, const char* message);
-void osdialog_message_async(int level, int buttons, const char* message, osdialog_message_callback* cb, void* user);
 const char* osdialog_prompt(int level, const char* message, const char* text);
-typedef void osdialog_prompt_callback(char* result, void* user);
-void osdialog_prompt_async(int level, const char* message, const char* text, osdialog_prompt_callback* cb, void* user);
 typedef struct osdialog_filter_patterns {
 	char* pattern;
 	struct osdialog_filter_patterns* next;
@@ -38,20 +34,4 @@ typedef struct osdialog_filters {
 	osdialog_filter_patterns* patterns;
 	struct osdialog_filters* next;
 } osdialog_filters;
-osdialog_filters* osdialog_filters_parse(const char* str);
-void osdialog_filter_patterns_free(osdialog_filter_patterns* patterns);
-void osdialog_filters_free(osdialog_filters* filters);
-osdialog_filters* osdialog_filters_copy(const osdialog_filters* src);
 const char* osdialog_file(int action, const char* dir, const char* filename, const osdialog_filters* filters);
-typedef void osdialog_file_callback(const char* result, void* user);
-void osdialog_file_async(int action, const char* dir, const char* filename, const osdialog_filters* filters, osdialog_file_callback* cb, void* user);
-typedef struct {
-	unsigned char r, g, b, a;
-} osdialog_color;
-int osdialog_color_picker(osdialog_color* color, int opacity);
-typedef void osdialog_color_picker_callback(int result, osdialog_color color, void* user);
-void osdialog_color_picker_async(osdialog_color color, int opacity, osdialog_color_picker_callback* cb, void* user);
-typedef void* osdialog_save_callback(void);
-typedef void osdialog_restore_callback(void* ptr);
-void osdialog_set_save_callback(osdialog_save_callback* cb);
-void osdialog_set_restore_callback(osdialog_restore_callback* cb);
