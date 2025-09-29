b9bcfd0b8195640a8eb431dd3f68ae418b804b3a
KingBes <48203677+KingBes@users.noreply.github.com>
2025-09-29 11:16:47 +0800

    Update pebview.php

--- src/config/plugin/kingbes/pebview/pebview.php
+++ src/config/plugin/kingbes/pebview/pebview.php
@@ -3,29 +3,36 @@
 use Kingbes\PebView\WindowHint;
 
 return [
-    "debug" => true,
-    "title" => "PebView",
-    "size" => [640, 480, WindowHint::None],
-    "icon" => base_path() . "/public/favicon.ico",
-    "closeCallback" => function ($win) {
+    "debug" => true, // 是否开启调试模式
+    "title" => "PebView", // 窗口标题
+    "size" => [640, 480, WindowHint::None], // 窗口大小
+    "icon" => base_path() . "/public/favicon.ico", // 窗口图标
+    "closeCallback" => function ($win) { // 窗口关闭回调
         $win->hide();
     },
-    "tray" => [
-        "icon" => base_path() . "/public/favicon." . (PHP_OS_FAMILY === "Linux" ? "png" : "ico"),
-        "menu" => [
+    "tray" => [ // 系统托盘
+        "icon" => base_path() . "/public/favicon." . (PHP_OS_FAMILY === "Linux" ? "png" : "ico"), // 系统托盘图标
+        "menu" => [ // 系统托盘菜单
             [
-                "text" => "显示窗口",
-                "cb" => function ($win) {
+                "text" => "显示窗口", // 菜单名称
+                "cb" => function ($win) { // 菜单回调
                     $win->show();
                 }
             ],
             [
-                "text" => "退出应用",
-                "cb" => function ($win) {
+                "text" => "退出应用", // 菜单名称
+                "cb" => function ($win) { // 菜单回调
                     $win->terminate();
                 }
             ]
         ]
     ],
-    "bind" => []
+    "bind" => [ // 绑定js事件
+        [
+            "name" => "hello", // 事件名称
+            "cb" => function (...$params) { // 事件回调
+                return "hello";
+            }
+        ]
+    ]
 ];
