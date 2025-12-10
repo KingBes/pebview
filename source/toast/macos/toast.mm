6781d188b753b240891861d31cde2c66650ed601
KingBes <48203677+KingBes@users.noreply.github.com>
2025-12-10 15:26:06 +0800

    Update toast.mm

--- source/toast/macos/toast.mm
+++ source/toast/macos/toast.mm
@@ -8,46 +8,42 @@ bool toastShow(
     const char *message,
     const char *image_path)
 {
-    @autoreleasepool
-    {
+    @autoreleasepool {
         // 将 C 字符串转换为 NSString
-        NSString *nsApp = [NSString stringWithUTF8String:app ? app : ""];
-        NSString *nsTitle = [NSString stringWithUTF8String:title ? title : ""];
-        NSString *nsMessage = [NSString stringWithUTF8String:message ? message : ""];
-        NSString *nsImagePath = image_path ? [NSString stringWithUTF8String:image_path] : nil;
-
+        NSString* nsApp = [NSString stringWithUTF8String:app ? app : ""];
+        NSString* nsTitle = [NSString stringWithUTF8String:title ? title : ""];
+        NSString* nsMessage = [NSString stringWithUTF8String:message ? message : ""];
+        NSString* nsImagePath = image_path ? [NSString stringWithUTF8String:image_path] : nil;
+        
         // 创建通知对象
-        NSUserNotification *notification = [[NSUserNotification alloc] init];
+        NSUserNotification* notification = [[NSUserNotification alloc] init];
         notification.title = nsTitle;
         notification.informativeText = nsMessage;
         notification.soundName = NSUserNotificationDefaultSoundName;
-
+        
         // 设置应用名称
-        if (nsApp.length > 0)
-        {
+        if (nsApp.length > 0) {
             notification.subtitle = nsApp;
         }
-
+        
         // 设置通知图标
-        if (nsImagePath && nsImagePath.length > 0)
-        {
-            NSURL *imageURL = [NSURL fileURLWithPath:nsImagePath];
-            NSImage *image = [[NSImage alloc] initWithContentsOfURL:imageURL];
-            if (image)
-            {
+        if (nsImagePath && nsImagePath.length > 0) {
+            NSURL* imageURL = [NSURL fileURLWithPath:nsImagePath];
+            NSImage* image = [[NSImage alloc] initWithContentsOfURL:imageURL];
+            if (image) {
                 // 调整图标大小
                 [image setSize:NSMakeSize(64, 64)];
                 notification.contentImage = image;
             }
         }
-
+        
         // 发送通知
-        NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
+        NSUserNotificationCenter* center = [NSUserNotificationCenter defaultUserNotificationCenter];
         [center deliverNotification:notification];
-
+        
         // 清理资源
         [notification release];
-
+        
         return true;
     }
 }
